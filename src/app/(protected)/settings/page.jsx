d3ed5fc8807612dd545ad76037f186889a314526
Manav Bansal <manav@manav.com>
2024-07-30 14:54:45 +0530

    setting page updated againnnnn

--- src/app/(protected)/settings/page.jsx
+++ src/app/(protected)/settings/page.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { auth } from '@/auth'; // Ensure the correct path to your auth module
 import SignOutForm from '@/app/component/SignoutForm';
 
-
 const SettingsPage = async () => {
   const session = await auth();
 
   return (
     <div className='gap-x-10'>
       <pre>{JSON.stringify(session, null, 2)}</pre>
-    <SignOutForm />
+      <SignOutForm />
     </div>
   );
 };
